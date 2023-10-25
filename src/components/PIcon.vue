7d553701350dd8583aff032a460125011aa92cc3
simoneboccolari <simone.boccolari@mumbleideas.it>
2023-10-25 09:30:11 +0200

    fix: add accessibility label to icon

--- src/components/PIcon.vue
+++ src/components/PIcon.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
+import PText from '@/components/PText.vue'
+
 type Props = {
     icon?: any
+    accessibilityLabel?: string
 }
 
 withDefaults(defineProps<Props>(), {
     icon: undefined,
+    accessibilityLabel: undefined,
 })
 </script>
 
 <template>
-    <Component :is="icon" class="p-icon" />
+    <span>
+        <PText as="span" visually-hidden> {{ accessibilityLabel }} </PText>
+        <Component :is="icon" class="p-icon" />
+    </span>
 </template>
